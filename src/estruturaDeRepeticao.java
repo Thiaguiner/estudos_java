03a847065e30a5feea7026900f56ad9ce43f2cdd
Thiaguiner <tcarneiro990@gmail.com>
2024-04-10 00:28:35 -0300

    estrutura de repetição

--- src/estruturaDeRepeticao.java
+++ src/estruturaDeRepeticao.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class estrutura_repeticao {
+public class estruturaDeRepeticao {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
