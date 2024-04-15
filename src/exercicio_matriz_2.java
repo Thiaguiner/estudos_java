06a1b3fe2fae2853b778d1a3365bfdd22554c917
Thiaguiner <tcarneiro990@gmail.com>
2024-04-15 17:27:47 -0300

    exercicios resolvidos de matriz

---
+++ src/exercicio_matriz_2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class exercicio_matriz_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        double[][] numeros = new double[M][N];
+
+        for (int i=0; i<M; i++) {
+            for (int j=0; j<N; j++) {
+                numeros[i][j] = sc.nextDouble();
+            }
+        }
+        double[] vet = new double[M];
+        for (int i=0; i<M; i++) {
+            double soma = 0.0;
+            for (int j=0; j<N; j++) {
+                soma = soma + numeros[i][j];
+            }
+            vet[i] = soma;
+        }
+        for (int i=0; i<M; i++) {
+            System.out.printf("%.1f%n", vet[i]);
+        }
+
+        sc.close();
+    }
+}
